8d41cbe637e2eefafe38aa12c5c8772e8668561a
ArtVelych <143528173+ArtVelych@users.noreply.github.com>
2023-12-08 00:43:16 +0100

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,48 @@
+function filterArray(numbers, value) {
+    const array = [];
+
+    for (const element of numbers) {
+        if (element > value) {
+            array.push(element)
+        }
+    }
+    return array;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function checkForSpam(message) {
+//     const spamWord1 = "spam";
+//     const spamWord2 = "sale";
+//     return message.toLowerCase().includes(spamWord1.toLowerCase()) || message.toLowerCase().includes(spamWord2.toLowerCase());
+// }
+
+// console.log(checkForSpam("Latest technology news")); // false
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+// console.log(checkForSpam("Get best sale offers now!")); // true
+// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+// console.log(checkForSpam("Trust me, this is not a spam message")); // true
+// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
