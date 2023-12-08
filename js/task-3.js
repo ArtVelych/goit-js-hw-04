31a78d71e5e2dc3d90be3a8408ed9a2073063e06
ArtVelych <artemvelychko1999@gmail.com>
2023-12-08 01:49:18 +0100

    First code version

--- js/task-3.js
+++ js/task-3.js
@@ -1,48 +1,24 @@
-function filterArray(numbers, value) {
-    const array = [];
+const profile = {
+    username: "Jacob",
+    playTime: 300,
 
-    for (const element of numbers) {
-        if (element > value) {
-            array.push(element)
-        }
-    }
-    return array;
-}
+    changeUsername(newName) {
+        this.username = newName;
+    },
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
 
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+};
 
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function checkForSpam(message) {
-//     const spamWord1 = "spam";
-//     const spamWord2 = "sale";
-//     return message.toLowerCase().includes(spamWord1.toLowerCase()) || message.toLowerCase().includes(spamWord2.toLowerCase());
-// }
-
-// console.log(checkForSpam("Latest technology news")); // false
-// console.log(checkForSpam("JavaScript weekly newsletter")); // false
-// console.log(checkForSpam("Get best sale offers now!")); // true
-// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-// console.log(checkForSpam("Trust me, this is not a spam message")); // true
-// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
